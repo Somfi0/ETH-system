ce2f1088a739899ce2ef110f512bff7d2fbbb494
Somfi0 <78470244+Somfi0@users.noreply.github.com>
2021-04-06 19:56:07 +0200

    me

--- commands/warn/clearw.js
+++ commands/warn/clearw.js
@@ -8,6 +8,9 @@ module.exports = {
 
         const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         if(!user) return message.channel.send('Вы не выбрали участника')
+        if(message.member.roles.highest.position <= user.roles.highest.position) return message.channel.send(`${message.author} вы не можете дать предупреждение кому то из модерации`)
+        if(message.member.roles.highest.position == user.roles.highest.position) return message.channel.send(`${message.author} вы не можете дать предупреждение кому то из модерации`)
+        if(message.author.id == user.id) return message.channel.send(`${message.author} Вы не можете использовать эту команду на себе`)
 
         db.findOne({ guildid : message.guild.id, user: user.user.id}, async(err,data) => {
             if(err) throw err;
--- commands/warn/rwarn.js
+++ commands/warn/rwarn.js
@@ -3,9 +3,14 @@ const db = require('../../models/wans')
 module.exports = {
     name : 'rwarn',
     run : async(client, message, args) => {
-        if(!message.member.permissions.has('MANAGE_MESSAGES')) return message.channel.send('У вас не достаточно прав на использование данной команды')        
+        if(!message.member.permissions.has('MANAGE_MESSAGES')) return message.channel.send('У вас не достаточно прав на использование данной команды')  
+              
         const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         if(!user) return message.channel.send('Вы не выбрали участника')
+        if(message.member.roles.highest.position <= user.roles.highest.position) return message.channel.send(`${message.author} вы не можете дать предупреждение кому то из модерации`)
+        if(message.member.roles.highest.position == user.roles.highest.position) return message.channel.send(`${message.author} вы не можете дать предупреждение кому то из модерации`)
+        if(message.author.id == user.id) return message.channel.send(`${message.author} Вы не можете использовать эту команду на себе`)
+
         db.findOne({ guildid : message.guild.id, user: user.user.id}, async(err,data) => {
             if(err) throw err;
             if(data) {
